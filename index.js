082a3f0f973dc8cedb0ad57e34e9bc28bafd3de8
muhbintangr <muhbintangr19@gmail.com>
2024-02-15 16:42:53 +0700

    add items table and update invoices table

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ import db from './lib/db.js'
 const app = express();
 
 db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, email VARCHAR(225), password VARCHAR(225), role VARCHAR(225))");
-    db.run("CREATE TABLE IF NOT EXISTS invoices(id VARCHAR(225) PRIMARY KEY, email VARCHAR(225), status VARCHAR(225), priority VARCHAR(225))");
+    db.run("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username VARCHAR(255), email VARCHAR(225), password VARCHAR(225), role VARCHAR(225), created_at VARCHAR(225))");
+    db.run("CREATE TABLE IF NOT EXISTS invoices(id VARCHAR(225) PRIMARY KEY, fullname VARCHAR(225), email VARCHAR(225), note TEXT, status VARCHAR(225), priority VARCHAR(225), created_at VARCHAR(225), updated_at VARCHAR(225))");
+    db.run("CREATE TABLE IF NOT EXISTS items(id VARCHAR(225) PRIMARY KEY, QB_quantity INTEGER, QB_price INTEGER, QB_amount INTEGER, QK_quantity INTEGER, QK_price INTEGER, QK_amount INTEGER, GN_amount INTEGER, total INTEGER, invoice_id VARCHAR(225), FOREIGN KEY (invoice_id) REFERENCES invoices(id))");
     db.run("CREATE TABLE IF NOT EXISTS images(id INTEGER PRIMARY KEY, title VARCHAR(225), invoice_id VARCHAR(225), invoice_status VARCHAR(225), FOREIGN KEY (invoice_id) REFERENCES invoices(id), FOREIGN KEY (invoice_status) REFERENCES invoices(status))");
 });
 
@@ -32,8 +33,8 @@ app.post('/login', (req, res) => {
             if (row === undefined) return res.status(401).json({ success: false, message: 'User tidak ditemukan' })
 
             if (await bcrypt.compare(req.body.password, row.password)) {
-                const accessToken = jwt.sign({ email: row.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 10 })
-                return res.status(200).json({ success: true, message: 'Berhasil login', accessToken: accessToken, user: { email: row.email } })
+                const accessToken = jwt.sign({ email: row.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "2h" })
+                return res.status(200).json({ success: true, message: 'Berhasil login', accessToken: accessToken })
             } else {
                 return res.status(401).json({ success: false, message: 'Email atau password salah' })
             }
@@ -45,7 +46,8 @@ app.post('/login', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-    const { email, password } = req.body
+    const { username, email, password } = req.body
+    const date = new Date()
 
     try {
         db.get('SELECT * FROM users', async (err, row) => {
@@ -54,8 +56,8 @@ app.post('/register', (req, res) => {
     
             const hashedPassword = await bcrypt.hash(password, 10)
             
-            db.run(`INSERT INTO users (email, password) VALUES (?,?)`, 
-                [ email, hashedPassword], (err) => {
+            db.run(`INSERT INTO users (username, email, password, role, created_at) VALUES (?,?,?,'admin', '${date.toISOString()}')`, 
+                [ username, email, hashedPassword], (err) => {
                     if (err) return res.status(401).json({ success: false })
                     return res.status(201).json({ success: true, message: 'user registered!' })
             })
